6e33a590835770d138f3e0821610ecf172df8522
OmarElkasrawy <OA2101689@tkh.edu.eg>
2024-03-15 14:58:47 +0200

    commit algorithm encryption

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cstdlib>
+
+using namespace std;
+
+class passwordManager {
+private:
+    unordered_map<string, string> users; // thats a way to store usernames and passwords
+
+    // now use Caeser
+    string encrypt(const string& password, int shift) {
+        string encrypted = password;
+        for (char& c : encrypted) {
+            if (isalpha(c)) {
+                c = (c + shift - 'a') % 26 + 'a';
+            }
+        }
+        return encrypted;
+    }
+
+    // now decrypt Caeser
+    string decrypt(const string& encrypted, int shift) {
+        string decrypted = encrypted;
+        for (char& c : decrypted) {
+            if (isalpha(c)) {
+                c = (c - shift - 'a' + 26) % 26 + 'a';
+            }
+        }
+        return decrypted;
+    }
+
+};
+
+
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* References
+ * unordered_map https://www.geeksforgeeks.org/unordered_map-in-cpp-stl/
+ * ctime https://cplusplus.com/reference/ctime/
+ * cstdlib https://cplusplus.com/reference/cstdlib/
+ *
+ *
+ *
+ * */
